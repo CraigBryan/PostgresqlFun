8610f770ea1572d3e24b5a078db4552a563fa1f7
Kat Drobnjakovic <kat.drobnjakovic@gmail.com>
2014-11-06 15:07:16 -0500

    Updated sql

--- testsuite_CSI3130/testcase_sql.sql
+++ testsuite_CSI3130/testcase_sql.sql
@@ -35,12 +35,16 @@ CREATE TABLE table4
 (
 id int, 
 name varchar(255), 
-startDate date, 
-dateOfBirth date, 
 country varchar(255)
 );
 
-SELECT table3.id, table3.name, table3.startDate, table3.dateOfBirth, table3.country
+SELECT table3.id, table3.name, table3.startDate, table3.dateOfBirth, table3.country, table4.id, table4.country, table4.name
 FROM table3
 JOIN table4 
-ON t1.name = t2.name;
+ON table3.id = table4.id AND table3.country = table4.country;
+
+SELECT table3.id, table3.name, table3.startDate, table3.dateOfBirth, table3.country, table4.id, table4.country, table4.name
+FROM table3
+FROM table3
+JOIN table4 
+ON table3.country = table4.country AND table3.name = table4.name;
