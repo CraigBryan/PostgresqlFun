e65d0c59c9fa951856e78596141953d768517c63
Kat Drobnjakovic <kat.drobnjakovic@gmail.com>
2014-11-06 12:53:42 -0500

    Added sql for test case

--- testsuite_CSI3130/testcase_sql.sql
+++ testsuite_CSI3130/testcase_sql.sql
@@ -15,11 +15,32 @@ name varchar(255),
 SELECT table1.id, table1.name, table2.id, table2.name
 FROM table1 
 JOIN table2 
-ON t1.id = t2.id;
+ON table1.id = table2.id;
 
 SELECT table1.id, table1.name, table2.id, table2.name
 FROM table1 
 JOIN table2 
+ON table1.name = table2.name;
+
+CREATE TABLE table3
+(
+id int, 
+name varchar(255), 
+startDate date, 
+dateOfBirth date, 
+country varchar(255)
+);
+
+CREATE TABLE table4
+(
+id int, 
+name varchar(255), 
+startDate date, 
+dateOfBirth date, 
+country varchar(255)
+);
+
+SELECT table3.id, table3.name, table3.startDate, table3.dateOfBirth, table3.country
+FROM table3
+JOIN table4 
 ON t1.name = t2.name;
-
-
