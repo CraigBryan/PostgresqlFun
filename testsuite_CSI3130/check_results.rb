b4b9e2a4ee7575eae0fa80003dd221e7d56bd725
Craig <craigbryan.csb@gmail.com>
2014-11-06 13:15:14 -0500

    Added a ruby script to compare .csv files. Added actual and expected folders.

--- testsuite_CSI3130/check_results.rb
+++ testsuite_CSI3130/check_results.rb
@@ -1,8 +1,32 @@
-tests = {fakeTest => ["fakeFile.csv", "fakeExpectedFile.csv"]}
+# CSI3130 Project Testing script
 
-test.each do |testName, files| {
-  actualFile = open(files[0])
-  expectedFile = open(files[1])
+expected_folder = "./expected/"
+actual_folder = "./actual/"
 
-  actualFile.
-}
+#To add a test, add to this hash
+tests = {"testing the tester" => ["#{actual_folder}fakeFile.csv", "#{expected_folder}fakeExpectedFile.csv"]}
+
+test_results = {}
+
+tests.each do |test_name, files| 
+
+  result = "Passed"
+
+  actual_lines = open(files[0]).readlines
+  expected_lines = open(files[1]).readlines
+
+  #Actual entry testing
+  actual_lines.each_index do |index| 
+    if expected_lines[index] != actual_lines[index]
+      result = "Failed"
+      break
+    end 
+  end
+
+  test_results[test_name] = result
+end
+
+#result output
+test_results.each do |test_name, result|
+  puts "#{test_name}: #{result}"
+end
